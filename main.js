0238dead3f279f289fa903d795680a512216a484
Vardan <70532109+Vardan2009@users.noreply.github.com>
2025-05-12 10:06:18 +0400

    Added code explanation

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
+// utility function: check if a character is alphanumeric 
 const isalnum = (char) => /^[a-zA-Z0-9]$/.test(char);
+
+// first step of compilers/interpreters, turning the
+// input stream into an array of tokens, smallest unit
+// in the code
+
+// Tokenization/Lexing example for LISP syntax
+// (add (mul 2 3) 1)
+// Lexer output:
+// ['(', 'add', '(', 'mul', '2', '3', ')', '1', ')']
 
 const tokenize = (src) => {
     const tokens = [];
@@ -20,6 +30,34 @@ const tokenize = (src) => {
 
     return tokens;
 };
+
+// the second step of compilers/interpretrrs is the parser
+// which is responsible for generating an Abstract Syntax Tree (AST),
+// A tree of the operations done by the code
+// the ast is made up of a root node, which is a list of other nodes,
+// in a typical lisp syntax, we don't need to care about operator precedence (e.g multiplication before addition)
+// but that needs to be handled in more advanced syntaxes in the parser
+
+// in this exact implementation, each node is an object with this
+// structure: {command: "command name, e.g add", operands: [{},{},{}]}
+// this is an example AST from the code (add (mul 2 3) 1), after lexing
+
+/*
+[ // root node, for each statement
+    { // add command
+        command: "add",
+        operands: [
+            { // mul commands, "child" of add command
+                command: "mul",
+                operands: [
+                    2, 3
+                ]
+            },
+            1
+        ]
+    }
+]
+*/
 
 const gen_ast = (tokens, tokenIdx = 0) => {
     const parse = () => {
@@ -53,6 +91,18 @@ const gen_ast = (tokens, tokenIdx = 0) => {
     return ast;
 };
 
+// this is a function which evaluates a node recursively,
+// recursion is used to support nested operations like the example above,
+// e.g in (add (mul 2 3) 1), the first step is executing the root statement (the add node)
+// (note: each node returns a value after evaluation)
+// using recursion, before rethrning the value of add, it evaluates the (mul 2 3) and 1 nodes,
+// and adding the results together, that's how the add command works
+
+// if we visualize the steps of the evaluation, this is how it will look
+// (add (mul 2 3) 1)
+// (add 6 1)
+// 7
+
 const eval_command = (cmd) => {
     if (typeof cmd !== "object") return cmd;
     switch (cmd.command) {
@@ -82,6 +132,7 @@ const eval_command = (cmd) => {
 
 console.log("JSLisp");
 
+// simple repl shell
 while (true) {
     const line = prompt("%");
     const tokens = tokenize(line);
